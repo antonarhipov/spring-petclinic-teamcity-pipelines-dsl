05ff0daa0e48711640d410b004ea214ae6efc90b
Anton Arhipov <ant.arhipov@gmail.com>
2019-05-08 13:54:50 +0300

    added the code as a preview reference

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,7 +26,62 @@ version = "2018.2"
 
 project(SpringPetclinic)
 
-
+// MOVED THE CONFIGURATION OBJECTS TO settings/SpringPetclinic.kt
+// The code below is _still_ here for demo purpose
+//
+//object SpringPetclinic : Project({
+//    vcsRoot(PetclinicVcs)
+//
+//    sequence {
+//        build(Build) {
+//            produces("target/spring-petclinic-*.jar => spring-petclinic.zip")
+//        }
+//        build(Upload){
+//            requires(Build, "spring-petclinic.zip")
+//        }
+//    }
+//})
+//
+//object Build : BuildType({
+//    name = "Build"
+//
+//    vcs {
+//        root(PetclinicVcs)
+//    }
+//
+//    steps {
+//        maven {
+//            goals = "clean package"
+//            dockerImage = "maven:3.6.0-jdk-8"
+//        }
+//    }
+//})
+//
+//object Upload : BuildType({
+//    name = "Upload"
+//
+//    steps {
+//        step {
+//            type = "ftp-deploy-runner"
+//            param("jetbrains.buildServer.deployer.ftp.authMethod", "ANONYMOUS")
+//            param("jetbrains.buildServer.deployer.ftp.transferMethod", "AUTO")
+//            param("jetbrains.buildServer.deployer.sourcePath", "spring-petclinic.zip")
+//            param("jetbrains.buildServer.deployer.targetUrl", "some.host.com")
+//            param("jetbrains.buildServer.deployer.ftp.securityMode", "0")
+//        }
+//    }
+//
+//    triggers {
+//        finishBuildTrigger {
+//            buildType = "${Build.id}"
+//        }
+//    }
+//})
+//
+//object PetclinicVcs : GitVcsRoot({
+//    name = "PetclinicVcs"
+//    url = "https://github.com/spring-projects/spring-petclinic.git"
+//})
 
 //
 //fun wrapWithFeature(buildType: BuildType, featureBlock: BuildFeatures.() -> Unit): BuildType {
